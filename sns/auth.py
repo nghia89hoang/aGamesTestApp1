63e0fb9a2ed2117ac99eff55f2157d40c4c0d9a8
stealthmoon <nghia89hoang@gmail.com>
2015-11-17 10:00:43 +0700

    - 1st try hard

--- sns/auth.py
+++ sns/auth.py
@@ -7,6 +7,8 @@ from config import CONFIG
 authomatic = Authomatic(config=CONFIG, secret='some random secret string')
 
 class SnsAuth(webapp2.RequestHandler):
-  pass
-
+  def get(self)
+    result = authomatic.login(Webapp2Adapter(self), 'fb')
+    if result:
+      self.response.write('l s')
 app = webapp2.WSGIApplication([('/sns', SnsAuth)], debug=True)
--- sns/config.py
+++ sns/config.py
@@ -3,8 +3,8 @@ from authomatic.provider import oauth2, openid, gaeopenid
 CONFIG = {
 	'fb': {
 		'class' : oauth2.Facebook,
-		'consumer_key' : '',
-		'consumer_secret': '',
+		'consumer_key' : '249629475103444',
+		'consumer_secret': 'd24204ed6d5fe592ad63f2f856e46f07',
 		'scope': ['user_about_me', 'email', 'publish_stream', 'read_stream']
 	}
 }
